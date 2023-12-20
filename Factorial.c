9beea44c2cfe29e3832128261d1ddf2a1a837398
BathuPadmanabhan <152837672+BathuPadmanabhan@users.noreply.github.com>
2023-12-20 18:34:46 +0530

    Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
 
+int fact(int x)
+{
+	int f=1,i;
+	if(x<=0)
+	{
+		return 1;
+	}
+	else
+	{
+		for(i=x;i>0;i--)
+		{
+			f = f*i;
+		}
+	}
+	
+	return f;
+}
+
+
+int main()
+{
+	int num;
+	printf("Enter a Number : ");
+	scanf("%d",&num);
+	
+	fact(num);
+	printf("\nThe Factorial of %d is : %d",num,fact(num));
+	printf("\n\nDone By Padmanabhan S\n192324076");
+	
+	
+}
