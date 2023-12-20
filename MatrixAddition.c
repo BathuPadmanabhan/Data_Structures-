9012928581d699e5987b3252a505e1c0cc6266f1
BathuPadmanabhan <152837672+BathuPadmanabhan@users.noreply.github.com>
2023-12-20 21:25:23 +0530

    MatrixAddition.c

---
+++ MatrixAddition.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+	int m1[2][2] = {{1,2},
+	                {3,4}};
+	int m2[2][2] = {{1,2},
+	    			{3,4}};
+	int sum=0,i,j;
+	
+	printf("THe Matrix : \n");
+	for(i=0;i<2;i++)
+	{
+		
+		for(j=0;j<2;j++)
+		{
+			printf("%d ",m1[i][j]);
+			
+			sum += m1[i][j]*m2[i][j];
+		}
+		printf("\n");
+	}
+	printf("Ths Sum : %d",sum);
+}
