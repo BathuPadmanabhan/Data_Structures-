82d0bd2cf90d460b182179db36e49578d76d0ace
BathuPadmanabhan <152837672+BathuPadmanabhan@users.noreply.github.com>
2023-12-21 13:43:18 +0530

    Ascending&Descending.c

---
+++ Ascending&Descending.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+void main()
+{
+    int i,k,j,temp;
+    int a[] = {8,1,4,9,1,3};
+    int n = sizeof(a)/sizeof(a[0]);
+    for(i=0;i<n;i++)
+     {
+           for(j=0;j<n;j++)
+           {
+              if(a[i]>a[j])
+              {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+              }
+           }
+           
+     }
+     printf("Descending Order : ");
+     for(k=0;k<n;k++)
+     {
+       printf("%d ",a[k]);
+     }
+     for(i=0;i<n;i++)
+     {
+           for(j=0;j<n;j++)
+           {
+              if(a[i]<a[j])
+              {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+              }
+           }
+           
+     }
+     printf("\nAscending Order : ");
+     for(k=0;k<n;k++)
+     {
+       printf("%d ",a[k]);
+     }
+     
+
+
+}
