e8f97b9a47877eea1ab3c7e4e8b473279c48123f
Padmanabhan S <152837672+BathuPadmanabhan@users.noreply.github.com>
2023-12-25 12:42:40 +0530

    Linked_List.c

--- Linked_List*.c
+++ Linked_List*.c
@@ -26,7 +26,7 @@ void in_end (int num)
    n = (struct node*)malloc(sizeof(struct node));
    n->data = num;
    n->next = NULL;
-   if( head == num)
+   if( head->data == num)
    {
     head=n;
     tail=n;
@@ -64,7 +64,7 @@ void de_be()
 void de_end()
 {
     struct node *t,*tp;
-    for(t=head;t!=NULL;t = t->tail)
+    for(t=head;t!=NULL;t = NULL)
     {
       tp = t;
       t =t ->next;
@@ -74,7 +74,7 @@ void de_end()
     tail->next = NULL;
     free(t);
 }
-void del_mid(int pos){
+void de_mid(int pos){
 	struct node *t,*tp;
 	t=head;
 	while(t->data!=pos){
@@ -84,10 +84,10 @@ void del_mid(int pos){
 	tp->next=t->next;
 	free(t);
 }
-void search(int key){
+void search(int n){
 	struct node *t;
 	for(t=head;t!=NULL;t=t->next){
-		if(t->data==key){
+		if(t->data==n){
 			printf("\nElement found at %d",t);
 			break;
 		}
@@ -97,10 +97,10 @@ void search(int key){
         }
     }
 }
-void bubbleSort() {
+void sort() {
     int swapped;
-    struct Node* first;
-    struct Node* last = NULL;
+    struct node* first;
+    struct node* last = NULL;
 
     // Checking for empty list
     if (head == NULL)
@@ -163,53 +163,68 @@ printf("The Array : ");
 display();
     printf("\n1 Insert a Number\n2 Insert at End\n3 Insert at Middle\n");
     printf("4 Delete at First\n5 Delete at End\n6 Delete at middle");
-    printf("\n7 SEARCH\n8 Sort the Array\n9 Find Minimum and Maximum");
-    int c,num;
+    printf("\n7 SEARCH\n8 Sort the Array\n9 Find Minimum and Maximum\n10 End");
+    int c,num,i;
     printf("\n\nEnter your choice : ");
+    while(c>0)
+    {
     scanf("%d",&c);
- scanf("%d",&num);
     switch(c)
         {
             case 1:
+                printf("Enter a number : ");
                  scanf("%d",&num);
                 in_be(num);
                 display();
                 break;
             case 2:
+                printf("Enter a number : ");
                  scanf("%d",&num);
                 in_end(num);
                 display();
                 break;
             case 3:
+                printf("Enter a number : ");
                  scanf("%d",&num);
                 in_mid(num);
                 display();
                 break;
             case 4:
+                printf("Enter a number : ");
                  scanf("%d",&num);
                 de_be(num);
                 display();
                 break;
             case 5:
+                printf("Enter a number : ");
                  scanf("%d",&num);
                 de_end(num);
                 display();
                 break;
             case 6:
+                 printf("Enter a number : ");
                  scanf("%d",&num);
                 de_mid(num);
                 display();
                 break;
             case 7:
-                 int pos;
-                 printf("\nEnter position : ");
-                 scanf("%d",&pos);
-                 search();
+                 int n;
+                 printf("\nEnter element : ");
+                 scanf("%d",&n);
+                 search(n);
+                 break;
             case 8:
                 sort();
                 display();
+                break;
             case 9:
                 min_max();
-   display();
+                display();
+                break;
+            case 10:
+                return 0;
    
+        }
+    }
+    return 0;
 }
