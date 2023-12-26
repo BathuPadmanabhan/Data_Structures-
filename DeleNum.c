9f2ac97d55809b64c329fd9dc8f5403e7d21afd9
Padmanabhan S <152837672+BathuPadmanabhan@users.noreply.github.com>
2023-12-27 00:19:45 +0530

    1_DeleNum.c

--- DeleNum.c
+++ DeleNum.c
@@ -3,27 +3,29 @@
 int main() {
   int arr[] = {1, 2, 3, 4, 5};
   int s = sizeof(arr)/sizeof(arr[0]);
-  int Delete ;
+  int Delete,i ;
   printf("Enter a value to delete : ");
   scanf("%d",&Delete);
-
+  printf("\nbefore : ");
   int index = -1;
-  for (int i = 0; i < s; i++) {
+  for (i = 0; i < s; i++) {
+  	printf("%d ",arr[i]);
     if (arr[i] == Delete) {
       index = i;
       break;
     }
   }
+  printf("\n");
 
- 
+
   if (index != -1) {
-    for (int i = index; i < 4; i++) {
+    for (i = index; i < 4; i++) {
       arr[i] = arr[i + 1];
     }
   }
 
-  // Print the updated array.
-  for (int i = 0; i < s; i++) {
+
+  for (i = 0; i < s; i++) {
     printf("%d ", arr[i]);
   }
 
